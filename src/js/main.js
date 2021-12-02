ed174b7837b3f7c319d0ba85db8206d0d0797f67
BaptisteDombard <91158751+BaptisteDombard@users.noreply.github.com>
2021-12-02 14:30:54 +0100

    init

--- src/js/main.js
+++ src/js/main.js
@@ -3,15 +3,17 @@
         init(){
             document.documentElement.classList.add("js-enabled");
 
-            const audios = new Map();
-            const aAudios = document.querySelectorAll("audio");
-            const aKeys = document.querySelectorAll(".key");
+            this.audios = new Map();
+            this.aAudios = document.querySelectorAll("audio");
+            this.aKeys = document.querySelectorAll(".key");
 
-            for (const eAudio of aAudios) {
-                audios.set(eAudio.dataset.key, eAudio);
+            for (const eAudio of this.aAudios) {
+                this.audios.set(eAudio.dataset.key, eAudio);
             }
-
-            for (const eKey of aKeys) {
+            this.addEventListener()
+        },
+        addEventListener (){
+            for (const eKey of this.aKeys) {
                 eKey.addEventListener("transitionend", (event)=>{
                     event.currentTarget.classList.remove("playing");
                     document.body.className = "";
@@ -20,19 +22,19 @@
                     playSound(event.currentTarget.dataset.key);
                 })
             }
-// console.log(audios);
-// console.log(audios.has("q"));
+            // console.log(audios);
+            // console.log(audios.has("q"));
             document.addEventListener("keydown", (event)=>{
-                playSound(event.key);
+                this.playSound(event.key);
             })
-            function playSound(key){
-                if (audios.has(key)){
-                    audios.get(key).currentTime = 0;
-                    audios.get(key).play();
-                    document.body.className = key;
-                    document.querySelector(`[data-key="${key}"]`).classList.add(`playing`);
-                    // console.log(document.querySelector(`[data-key="${event.key}"]`));
-                }
+        },
+        playSound(key){
+            if (this.audios.has(key)){
+                this.audios.get(key).currentTime = 0;
+                this.audios.get(key).play();
+                document.body.className = key;
+                document.querySelector(`[data-key="${key}"]`).classList.add(`playing`);
+                // console.log(document.querySelector(`[data-key="${event.key}"]`));
             }
         }
     }
