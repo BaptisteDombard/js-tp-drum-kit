c4ff4556c478144ec9616e90a511850aa07445af
BaptisteDombard <91158751+BaptisteDombard@users.noreply.github.com>
2021-12-02 14:18:05 +0100

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,29 +1,45 @@
-document.documentElement.classList.add("js-enabled");
+(function (){
+    const oDrum = {
+        init(){
+            document.documentElement.classList.add("js-enabled");
 
-const audios = new Map();
-const aAudios = document.querySelectorAll("audio");
-const aKeys = document.querySelectorAll(".key");
+            const audios = new Map();
+            const aAudios = document.querySelectorAll("audio");
+            const aKeys = document.querySelectorAll(".key");
 
-for (const eAudio of aAudios) {
-    audios.set(eAudio.dataset.key, eAudio);
-}
+            for (const eAudio of aAudios) {
+                audios.set(eAudio.dataset.key, eAudio);
+            }
 
-for (const eKey of aKeys) {
-    eKey.addEventListener("transitionend", (event)=>{
-        event.currentTarget.classList.remove("playing");
-        document.body.className = "";
-    })
-}
+            for (const eKey of aKeys) {
+                eKey.addEventListener("transitionend", (event)=>{
+                    event.currentTarget.classList.remove("playing");
+                    document.body.className = "";
+                });
+                eKey.addEventListener("click", (event)=>{
+                    playSound(event.currentTarget.dataset.key);
+                })
+            }
 // console.log(audios);
 // console.log(audios.has("q"));
-document.addEventListener("keydown", (event)=>{
-    if (audios.has(event.key)){
-        audios.get(event.key).play();
-        document.body.className = event.key;
-        document.querySelector(`[data-key="${event.key}"]`).classList.add(`playing`);
-        // console.log(document.querySelector(`[data-key="${event.key}"]`));
+            document.addEventListener("keydown", (event)=>{
+                playSound(event.key);
+            })
+            function playSound(key){
+                if (audios.has(key)){
+                    audios.get(key).currentTime = 0;
+                    audios.get(key).play();
+                    document.body.className = key;
+                    document.querySelector(`[data-key="${key}"]`).classList.add(`playing`);
+                    // console.log(document.querySelector(`[data-key="${event.key}"]`));
+                }
+            }
+        }
     }
-})
+    oDrum.init();
+})();
+
+
 
 
 
